75434d780d1b4fdf9f1f960a5547b6d8f82242c8
Robert Salaiz <work.salaizrm@gmail.com>
2020-04-24 23:27:40 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,6 +23,11 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
 
+  get '/recipes/:id/edit' do
+    @recipes = Recipe.find_by(params[:id])
+    erb :edit
+  end
+
  post '/recipes' do
    @recipes = Recipe.new(name: params[:name],ingredients: params[:ingredients],cook_time: params[:cook_time])
      @recipes.save
